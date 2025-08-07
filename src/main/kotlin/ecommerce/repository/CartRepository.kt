97b0ad642147f97929aa3a4a395af97c6f0b3aa3
Jin Park <jieun.park.e@deliveryhero.com>
2025-08-07 20:29:44 +0200

    fix: replace `JPQL queries` with `JPA's name-based queries` - findByMemberId() - could be method name based - findByIdAndMemberId() - could be method name based - findByMemberIdAndCartItemProductOptionId() - JPQL makes sense - deleteByMemberId() - could be method name based

--- src/main/kotlin/ecommerce/repository/CartRepository.kt
+++ src/main/kotlin/ecommerce/repository/CartRepository.kt
@@ -4,14 +4,10 @@ import ecommerce.model.Cart
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import org.springframework.stereotype.Repository
 
-@Repository
 interface CartRepository : JpaRepository<Cart, Long> {
-    @Query("SELECT c FROM Cart c WHERE c.member.id = :memberId")
     fun findByMemberId(memberId: Long): Cart?
 
-    @Query("SELECT c FROM Cart c WHERE c.id = :cartId AND c.member.id = :memberId")
     fun findByIdAndMemberId(
         cartId: Long,
         memberId: Long,
@@ -24,6 +20,5 @@ interface CartRepository : JpaRepository<Cart, Long> {
     ): Cart?
 
     @Modifying
-    @Query("DELETE FROM Cart c WHERE c.member.id = :memberId")
     fun deleteByMemberId(memberId: Long)
 }
