4c64bb4f85cec9f0568baa1ff258074cfa527bbb
Jin Park <jieun.park.e@deliveryhero.com>
2025-08-07 20:47:53 +0200

    style: add newline for better readability

--- src/main/kotlin/ecommerce/repository/CartItemRepository.kt
+++ src/main/kotlin/ecommerce/repository/CartItemRepository.kt
@@ -6,6 +6,7 @@ import ecommerce.model.ProductOption
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+
 interface CartItemRepository : JpaRepository<CartItem, Long> {
     fun findByCartAndProductOption(
         cart: Cart,
