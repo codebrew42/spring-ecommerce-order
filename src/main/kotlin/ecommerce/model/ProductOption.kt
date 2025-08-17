521d91bd5010ebe080aaaf902b35f9f9d3f58924
Jin Park <jieun.park.e@deliveryhero.com>
2025-08-17 15:56:02 +0200

    Add(ProductOption): Update entity

--- src/main/kotlin/ecommerce/model/ProductOption.kt
+++ src/main/kotlin/ecommerce/model/ProductOption.kt
@@ -6,19 +6,13 @@ import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
-import jakarta.persistence.Index
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import java.util.Objects
 
 @Entity
-@Table(
-    name = "product_options",
-    indexes = [
-        Index(name = "idx_product_option_product_id", columnList = "product_id"),
-    ],
-)
+@Table(name = "product_options")
 class ProductOption(
     @Column(name = "name", nullable = false)
     var name: String,
