76c6b02a07c332ce80531fb4397acff498ae5050
codebrew42 <jieun.park.e@deliveryhero.com>
2025-08-26 21:02:29 +0200

    chore: Update quantity request

--- src/main/kotlin/ecommerce/dto/cart/UpdateQuantityRequest.kt
+++ src/main/kotlin/ecommerce/dto/cart/UpdateQuantityRequest.kt
@@ -3,6 +3,6 @@ package ecommerce.dto.cart
 import jakarta.validation.constraints.Min
 
 data class UpdateQuantityRequest(
-    @field:Min(value = 1, message = "Quantity must be at least 1")
+    @field:Min(value = 0, message = "Quantity must be at least 0")
     val quantity: Int,
 )
